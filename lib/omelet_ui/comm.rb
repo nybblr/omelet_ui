743b2c6b1e9a517a3799898554579bc2b5823f04
Jonathan Martin <me@nybblr.com>
2012-10-01 13:01:46 -0400

    Initial version of the omelet ui comm library for clean JSON requests.

---
+++ lib/omelet_ui/comm.rb
@@ -0,0 +1,34 @@
+require 'net/http'
+require 'json'
+
+module OmeletUi
+	class Comm
+		def request(url, data, method)
+			uri = URI.parse(url)
+
+			# Convert the parameters into JSON and set the content type as application/json
+			req = method.new(uri.path)
+			req.body = JSON.generate(data)
+			req["Content-Type"] = "application/json"
+
+			http = Net::HTTP.new(uri.host, uri.port)
+			response = http.start {|h| h.request(req) }
+		end
+
+		def post(url, data)
+			request url, data, Net::HTTP::Post
+		end
+
+		def put(url, data)
+			request url, data, Net::HTTP::Put
+		end
+
+		def delete(url, data)
+			request url, data, Net::HTTP::Delete
+		end
+
+		def get(url, data)
+			request url, data, Net::HTTP::Get
+		end
+	end
+end
