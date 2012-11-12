e637afc1644490c2024ce9a97364ef887430eb9a
Jonathan Martin <me@nybblr.com>
2012-11-12 12:55:45 -0500

    Controllers and views for templates.

--- app/controllers/omelet_ui/templates_controller.rb
+++ app/controllers/omelet_ui/templates_controller.rb
@@ -3,13 +3,15 @@ require_dependency "omelet_ui/application_controller"
 module OmeletUi
 	class TemplatesController < ApplicationController
 		def index
-			@template = Template.new
+			@templates = Template.scoped
 		end
 
 		def show
+			@template = Template.find params[:id]
 		end
 
 		def new
+			@template = Template.new
 		end
 
 		def create
@@ -20,9 +22,15 @@ module OmeletUi
 		end
 
 		def edit
+			@template = Template.find params[:id]
 		end
 
 		def update
+			@template = Template.find params[:id]
+			@template.attributes = params[:template]
+			@template.save
+
+			redirect_to templates_path
 		end
 
 	end
