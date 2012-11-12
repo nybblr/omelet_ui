7256a66895889286742731d6948dae0b94657919
Jonathan Martin <me@nybblr.com>
2012-11-12 12:41:38 -0500

    Moving to haml, upping controller.

--- app/controllers/omelet_ui/templates_controller.rb
+++ app/controllers/omelet_ui/templates_controller.rb
@@ -1,17 +1,29 @@
 require_dependency "omelet_ui/application_controller"
 
 module OmeletUi
-  class TemplatesController < ApplicationController
-    def index
-    end
-  
-    def show
-    end
-  
-    def new
-    end
-  
-    def edit
-    end
-  end
+	class TemplatesController < ApplicationController
+		def index
+			@template = Template.new
+		end
+
+		def show
+		end
+
+		def new
+		end
+
+		def create
+			@template = Template.new params[:template]
+			@template.save
+
+			redirect_to templates_path
+		end
+
+		def edit
+		end
+
+		def update
+		end
+
+	end
 end
